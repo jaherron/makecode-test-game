56acbca092b12578d0b845f3b06d3e5388718664
jaherron <40708782+jaherron@users.noreply.github.com>
2020-07-05 18:47:33 -0500

    Added food and a button to regenerate food

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+namespace SpriteKind {
+    export const Button = SpriteKind.create()
+}
 controller.up.onEvent(ControllerButtonEvent.Released, function () {
     mySprite.setVelocity(0, 0)
 })
@@ -7,14 +10,41 @@ controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     mySprite.setVelocity(0, 50)
 })
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Button, function (sprite, otherSprite) {
+    mySprite2 = sprites.create(img`
+. . . . . . . . . . b b b . . . 
+. . . . . . . . b e e 3 3 b . . 
+. . . . . . b b e 3 2 e 3 a . . 
+. . . . b b 3 3 e 2 2 e 3 3 a . 
+. . b b 3 3 3 3 3 e e 3 3 3 a . 
+b b 3 3 3 3 3 3 3 3 3 3 3 3 3 a 
+b 3 3 3 d d d d 3 3 3 3 3 d d a 
+b b b b b b b 3 d d d d d d 3 a 
+b d 5 5 5 5 d b b b a a a a a a 
+b 3 d d 5 5 5 5 5 5 5 d d d d a 
+b 3 3 3 3 3 3 d 5 5 5 d d d d a 
+b 3 d 5 5 5 3 3 3 3 3 3 b b b a 
+b b b 3 d 5 5 5 5 5 5 5 d d b a 
+. . . b b b 3 d 5 5 5 5 d d 3 a 
+. . . . . . b b b b 3 d d d b a 
+. . . . . . . . . . b b b a a . 
+`, SpriteKind.Food)
+    mySprite2.setPosition(25, 20)
+})
 controller.left.onEvent(ControllerButtonEvent.Released, function () {
     mySprite.setVelocity(0, 0)
 })
 controller.down.onEvent(ControllerButtonEvent.Released, function () {
     mySprite.setVelocity(0, 0)
 })
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    info.changeLifeBy(1)
+    mySprite.say("*nom*", 500)
+    otherSprite.destroy()
+})
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
     info.changeLifeBy(-1)
+    mySprite.say("ouch", 500)
     mySprite.setPosition(77, 54)
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
@@ -34,6 +64,8 @@ info.onLifeZero(function () {
         mySprite.setPosition(77, 54)
     }
 })
+let mySprite3: Sprite = null
+let mySprite2: Sprite = null
 let enemy4: Sprite = null
 let enemy3: Sprite = null
 let enemy2: Sprite = null
@@ -144,5 +176,43 @@ if (game.ask("Enable enemies?", "")) {
 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
 `, SpriteKind.Enemy)
     enemy4.setPosition(77, 17)
+    mySprite2 = sprites.create(img`
+. . . . . . . . . . b b b . . . 
+. . . . . . . . b e e 3 3 b . . 
+. . . . . . b b e 3 2 e 3 a . . 
+. . . . b b 3 3 e 2 2 e 3 3 a . 
+. . b b 3 3 3 3 3 e e 3 3 3 a . 
+b b 3 3 3 3 3 3 3 3 3 3 3 3 3 a 
+b 3 3 3 d d d d 3 3 3 3 3 d d a 
+b b b b b b b 3 d d d d d d 3 a 
+b d 5 5 5 5 d b b b a a a a a a 
+b 3 d d 5 5 5 5 5 5 5 d d d d a 
+b 3 3 3 3 3 3 d 5 5 5 d d d d a 
+b 3 d 5 5 5 3 3 3 3 3 3 b b b a 
+b b b 3 d 5 5 5 5 5 5 5 d d b a 
+. . . b b b 3 d 5 5 5 5 d d 3 a 
+. . . . . . b b b b 3 d d d b a 
+. . . . . . . . . . b b b a a . 
+`, SpriteKind.Food)
+    mySprite2.setPosition(25, 20)
+    mySprite3 = sprites.create(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . 6 6 6 6 6 6 6 . . . . 
+. . . . 6 9 9 9 9 9 9 9 6 . . . 
+. . . 6 9 9 9 9 9 9 9 9 9 6 . . 
+. . 6 9 9 9 9 9 9 9 9 9 9 9 6 . 
+. . 6 9 9 9 9 9 9 9 9 9 9 9 6 . 
+. . 6 9 9 9 9 9 9 9 9 9 9 9 6 . 
+. . 6 9 9 9 9 9 9 9 9 9 9 9 6 . 
+. . 6 9 9 9 9 9 9 9 9 9 9 9 6 . 
+. . 6 9 9 9 9 9 9 9 9 9 9 9 6 . 
+. . 6 9 9 9 9 9 9 9 9 9 9 9 6 . 
+. . . 6 9 9 9 9 9 9 9 9 9 6 . . 
+. . . . 6 9 9 9 9 9 9 9 6 . . . 
+. . . . . 6 6 6 6 6 6 6 . . . . 
+. . . . . . . . . . . . . . . . 
+`, SpriteKind.Button)
+    mySprite3.setPosition(129, 19)
     info.setLife(3)
 }
